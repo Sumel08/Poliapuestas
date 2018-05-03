e67d72365609c6c54512b68afe81b101cfa87344
Oscar Lemus <oscarl.ocho@gmail.com>
2018-05-02 19:05:16 -0500

    feature: Add a black box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ColorComponent } from './color/color.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule
